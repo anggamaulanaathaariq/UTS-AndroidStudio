64f51435fb614a1f1ae8107a13119a88c3b938d5
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-10-10 19:10:44 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema/myapp/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ac.polinema.myapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.polinema.myapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/id/ac/polinema/myapp/fragments/BioFragment.java
@@ -0,0 +1,32 @@
+package id.ac.polinema.myapp.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.ac.polinema.myapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BioFragment extends Fragment {
+
+
+    public BioFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bio, container, false);
+    }
+
+}
